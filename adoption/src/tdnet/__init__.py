5ea331ac90c79e651481d9dd2f1eb4acd634a03d
Dave OR <orkapodjdj@gmail.com>
2026-01-18 09:15:02 +0800

    Improve

--- adoption/src/tdnet/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-TDnet Scrapers Package
-======================
-
-This package contains scrapers for TDnet:
-- announcement_scraper: For official announcements (English)
-- search_scraper: For searching third-party allotment deals (Japanese/Search)
-"""
