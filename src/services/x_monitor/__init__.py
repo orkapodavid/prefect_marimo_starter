f75f5e5c41a03ae8a63a76730c9f96264d551245
David OR <orkapodjdj@gmail.com>
2026-03-25 00:31:09 +0800

    feat: wire x monitor public API exports

--- src/services/x_monitor/__init__.py
+++ src/services/x_monitor/__init__.py
@@ -1,2 +1,23 @@
-"""X monitor services package."""
+"""Public API exports for X monitor services."""
 
+from services.x_monitor.x_monitor_config_loader import load_x_monitor_config
+from services.x_monitor.x_monitor_models import (
+    XMonitorConfig,
+    XMonitorMatchResult,
+    XMonitorNormalizedPost,
+    XMonitorNotificationPayload,
+    XMonitorRuntime,
+    XMonitorTarget,
+)
+from services.x_monitor.x_monitor_notifications import SendResult
+
+__all__ = [
+    "SendResult",
+    "XMonitorConfig",
+    "XMonitorMatchResult",
+    "XMonitorNormalizedPost",
+    "XMonitorNotificationPayload",
+    "XMonitorRuntime",
+    "XMonitorTarget",
+    "load_x_monitor_config",
+]
